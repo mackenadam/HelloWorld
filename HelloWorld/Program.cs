7fbf4a4805864fdbfb5c031d998862e2453a618d
Adam Macken <mackenadam@gmail.com>
2021-02-09 13:16:16 +1300

    Add note about floating number types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -17,12 +17,15 @@ namespace HelloWorld
             // Use Casting
             // int intOverDouble = num1 / d1;
 
+            float f1 = 6.9f;
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("The sum of integers like " + num1 + " and " + num2 + " is " + sum);
             Console.WriteLine("The sum of doubles like " + d1 + " and " + d2 + " is " + dSum);
             Console.WriteLine("You can even add integers like " + num1 + " and doubles like " + d1 + " to get " + (num1 + d1));
             Console.WriteLine("You can perform more advanced operations on like data types i.e " + d1 + " divided by " + d2 + " equals " + (d1 / d2));
             Console.WriteLine("However different data types like " + num1 + " and " + d1 + " will rely on 'casting'");
+            Console.WriteLine("Don't forget that floating number types like " + f1 + " need to have an f suffix when declared i.e  float f1 = " + f1 + "f");
             Console.Read();
         }
     }
