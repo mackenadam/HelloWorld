f3c5d6ab9201011cab0b8748b1f7632e746e734b
Adam Macken <mackenadam@gmail.com>
2021-02-09 13:10:11 +1300

    Introduce number data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,24 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            int num1 = 13;
+            int num2 = 31;
+            int sum = num1 + num2;
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double dSum = d1 + d2;
+
+            // Use Casting
+            // int intOverDouble = num1 / d1;
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine("The sum of integers like " + num1 + " and " + num2 + " is " + sum);
+            Console.WriteLine("The sum of doubles like " + d1 + " and " + d2 + " is " + dSum);
+            Console.WriteLine("You can even add integers like " + num1 + " and doubles like " + d1 + " to get " + (num1 + d1));
+            Console.WriteLine("You can perform more advanced operations on like data types i.e " + d1 + " divided by " + d2 + " equals " + (d1 / d2));
+            Console.WriteLine("However different data types like " + num1 + " and " + d1 + " will rely on 'casting'");
+            Console.Read();
         }
     }
 }
