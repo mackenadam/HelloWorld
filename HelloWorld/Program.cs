0b49130067032033bdd4211dd1e4fa792c85b6a3
Adam Macken <mackenadam@gmail.com>
2021-02-09 16:59:54 +1300

    Play with strings and basic String methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // Numbers
             int num1 = 13;
             int num2 = 31;
             int sum = num1 + num2;
@@ -19,6 +20,12 @@ namespace HelloWorld
 
             float f1 = 6.9f;
 
+            // Strings
+            string myName = "Adam";
+            string message = "Hey, " + myName + ", you can also use the String class methods on string variables. To make them all uppercase for instance...";
+            string message2 = "Or even all lowercase.";
+            string lowercaseMessage = message2.ToLower();
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("The sum of integers like " + num1 + " and " + num2 + " is " + sum);
             Console.WriteLine("The sum of doubles like " + d1 + " and " + d2 + " is " + dSum);
@@ -26,6 +33,9 @@ namespace HelloWorld
             Console.WriteLine("You can perform more advanced operations on like data types i.e " + d1 + " divided by " + d2 + " equals " + (d1 / d2));
             Console.WriteLine("However different data types like " + num1 + " and " + d1 + " will rely on 'casting'");
             Console.WriteLine("Don't forget that floating number types like " + f1 + " need to have an f suffix when declared i.e  float f1 = " + f1 + "f");
+            Console.WriteLine("Hello " + myName);
+            Console.WriteLine(message.ToUpper());
+            Console.WriteLine(lowercaseMessage);
             Console.Read();
         }
     }
